d5dd665bc8ae7e14f1e4c874148638a06f93baf2
chenzhi.xu(徐琛知) <chenzhi.xu@shopee.com>
2023-10-04 04:48:37 +0800

    feat: flash增加jump line,jump word

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -2,6 +2,24 @@ return {
     "folke/flash.nvim",
     ---@type Flash.Config
     opts = {
+        search = {
+            -- * exact: exact match
+            -- * search: regular search
+            -- * fuzzy: fuzzy search
+            -- * fun(str): custom function that returns a pattern
+            --   For example, to only match at the beginning of a word:
+            --   mode = function(str)
+            --     return "\\<" .. str
+            --   end,
+            node = "fuzzy"
+        },
+        jump = {
+            -- automatically jump when there is only one match
+            autojump = true,
+        },
+        label = {
+            uppercase = false,
+        },
         modes = {
             search = {
                 -- when `true`, flash will be activated during regular search by default.
@@ -13,8 +31,9 @@ return {
                 multi_line = false,
                 autohide = false
             }
-        }
+        },
     },
+
     -- stylua: ignore
     keys = {
         { "<c-s>", mode = { "n", "o", "x" }, function() require("flash").jump() end,              desc = "Flash" },
@@ -22,5 +41,66 @@ return {
         -- { "o",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
         { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
         { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+        {
+            "<leader>jl",
+            mode = { "n", "o", "x" },
+            function()
+                require("flash").jump({
+                    search = { mode = "search", max_length = 0 },
+                    label = { uppercase = true, after = { 0, 0 } },
+                    pattern = "^[ \t]*",
+                    jump = { pos = "end" },
+                })
+            end,
+            desc = "[J]ump [L]ine"
+        },
+        {
+            "<leader>jw",
+            mode = { "n", "o", "x" },
+            function()
+                require("flash").jump({
+                    pattern = ".", -- initialize pattern with any char
+                    search = {
+                        mode = function(pattern)
+                            -- remove leading dot
+                            if pattern:sub(1, 1) == "." then
+                                pattern = pattern:sub(2)
+                            end
+                            -- return word pattern and proper skip pattern
+                            return ([[\<%s\w*\>]]):format(pattern), ([[\<%s]]):format(pattern)
+                        end,
+                    },
+                    jump = {
+                        ---@type "start" | "end" | "range"
+                        pos = "start"
+                    },
+                })
+            end,
+            desc = "[J]ump [W]ord"
+        },
+        {
+            "<leader>jW",
+            mode = { "n", "o" },
+            function()
+                require("flash").jump({
+                    pattern = ".", -- initialize pattern with any char
+                    search = {
+                        mode = function(pattern)
+                            -- remove leading dot
+                            if pattern:sub(1, 1) == "." then
+                                pattern = pattern:sub(2)
+                            end
+                            -- return word pattern and proper skip pattern
+                            return ([[\<%s\w*\>]]):format(pattern), ([[\<%s]]):format(pattern)
+                        end,
+                    },
+                    jump = {
+                        ---@type "start" | "end" | "range"
+                        pos = "range"
+                    },
+                })
+            end,
+            desc = "[J]ump [W]ord and select"
+        },
     },
 }
