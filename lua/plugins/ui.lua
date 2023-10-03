08e0f6871efe53db811c7df253f304ddd19d6f7d
chenzhi.xu(徐琛知) <chenzhi.xu@shopee.com>
2023-10-03 22:46:29 +0800

    feat: 删除flash remote 命令,改alpha主题

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -15,8 +15,8 @@ return {
     {
         "goolord/alpha-nvim",
         config = function()
-            --require('alpha').setup(require('alpha.themes.startify').config)
-            require('alpha').setup(require('alpha.themes.dashboard').config)
+            require('alpha').setup(require('alpha.themes.startify').config)
+            -- require('alpha').setup(require('alpha.themes.dashboard').config)
         end
     },
     -- {
