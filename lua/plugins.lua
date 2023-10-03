c5377c1fd68f942cc8fc0ef080f637877ff8ec27
chenzhi.xu(徐琛知) <chenzhi.xu@shopee.com>
2023-10-03 18:46:38 +0800

    perf: boot perf

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,27 +2,27 @@ return {
     -- {'neoclide/coc.nvim', branch = 'release'},
     -- 'tpope/vim-fugitive'
     -- 插入模式下快捷键
-    { 'tpope/vim-rsi',          event = "VeryLazy", },
+    { 'tpope/vim-rsi',                   event = "VeryLazy", },
     -- 'scrooloose/nerdtree',
     -- 'jistr/vim-nerdtree-tabs',
     -- 'Xuyuanp/nerdtree-git-plugin',
     -- 'altercation/vim-colors-solarized',
     -- 'spf13/vim-colors',
     -- gr 匹配替换
-    'vim-scripts/ReplaceWithRegister',
+    { 'vim-scripts/ReplaceWithRegister', event = "VeryLazy", },
     -- sa sd sr
     -- i,I
-    'machakann/vim-sandwich',
+    { 'machakann/vim-sandwich',          event = "VeryLazy" },
     -- 快捷交换 cx cxc cxx
-    { 'tommcdo/vim-exchange',   event = "VeryLazy", },
-    { 'vim-scripts/DrawIt',     event = "VeryLazy", },
+    { 'tommcdo/vim-exchange',            event = "VeryLazy", },
+    { 'vim-scripts/DrawIt',              event = "VeryLazy", },
     -- f F t T 快捷移动提示
-    { 'unblevable/quick-scope', event = "VeryLazy", },
+    { 'unblevable/quick-scope',          event = "VeryLazy", },
     -- 'jiangmiao/auto-pairs',
     -- 'ctrlpvim/ctrlp.vim',
     -- 'tacahiroy/ctrlp-funky',
     -- <C-n> Ctrl-Down/Up Shift-Arrows n/N [/] q Q
-    { 'mg979/vim-visual-multi', branch = 'master',  event = "VeryLazy", },
+    { 'mg979/vim-visual-multi',          branch = 'master',  event = "VeryLazy", },
     -- i a I A   n l   aa ia
     -- 'wellle/targets.vim',
     -- { 'Lokaltog/powerline',     rtp = '/powerline/bindings/vim' },
@@ -40,23 +40,23 @@ return {
     -- crt Title Case
     -- :h abolish
     -- :%S/facilit{y, ies}/building{,s}/g
-    { 'tpope/vim-abolish',      event = "VeryLazy", },
+    { 'tpope/vim-abolish',                    event = "VeryLazy", },
     -- <ENTER> <BS>
     -- 'gcmt/wildfire.vim',
-    'voldikss/vim-floaterm',
-    'junegunn/fzf',
+    -- 'voldikss/vim-floaterm',
+    -- 'junegunn/fzf',
     -- Files GFiles GFiles? Buffers History History: History/ Helptags Filetypes Maps
-    'junegunn/fzf.vim',
+    -- 'junegunn/fzf.vim',
     -- ii ai aI
-    { 'michaeljsmith/vim-indent-object', event = "VeryLazy", },
+    { 'michaeljsmith/vim-indent-object',      event = "VeryLazy", },
     -- 'sindrets/diffview.nvim',
-    'christoomey/vim-tmux-navigator',
+    { 'christoomey/vim-tmux-navigator',       event = "VeryLazy" },
     -- gitsigns 替代
     -- 'airblade/vim-gitgutter',
-    'osyo-manga/vim-over',
-    'godlygeek/tabular',
-    { 'luochen1990/rainbow',             event = "VeryLazy", },
-    'SidOfc/mkdx',
+    { 'osyo-manga/vim-over',                  event = "VeryLazy" },
+    { 'godlygeek/tabular',                    event = "VeryLazy", },
+    { 'luochen1990/rainbow',                  event = "VeryLazy", },
+    { 'SidOfc/mkdx',                          event = "VeryLazy", },
     { 'tpope/vim-dadbod',                     event = "VeryLazy", },
     { 'kristijanhusak/vim-dadbod-ui',         event = "VeryLazy", },
     { 'kristijanhusak/vim-dadbod-completion', event = "VeryLazy", },
