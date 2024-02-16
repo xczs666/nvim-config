d5c1c896eebde5772064d7465f6e036febaf4a5a
chenzhi.xu(徐琛知) <chenzhi.xu@shopee.com>
2024-02-16 23:51:46 +0800

    feat: change flash key

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -36,7 +36,8 @@ return {
 
     -- stylua: ignore
     keys = {
-        { "<c-s>", mode = { "n", "o", "x" }, function() require("flash").jump() end,              desc = "Flash" },
+        -- { "<c-s>", mode = { "n", "o", "x" }, function() require("flash").jump() end,              desc = "Flash" },
+        { "s",     mode = { "n", "o", "x" }, function() require("flash").jump() end,              desc = "Flash" },
         { "S",     mode = { "n", "o", "x" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
         { "o",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
         { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
