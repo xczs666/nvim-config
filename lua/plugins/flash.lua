c351ccdc157bdb82c087bc1ab81c518159ff7b5e
chenzhi.xu(徐琛知) <chenzhi.xu@shopee.com>
2024-04-10 22:28:37 +0800

    ✨ feat: flash支持中文
    flash加入双拼码表支持

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,107 +1,134 @@
 return {
-    "folke/flash.nvim",
-    ---@type Flash.Config
-    opts = {
-        search = {
-            -- * exact: exact match
-            -- * search: regular search
-            -- * fuzzy: fuzzy search
-            -- * fun(str): custom function that returns a pattern
-            --   For example, to only match at the beginning of a word:
-            --   mode = function(str)
-            --     return "\\<" .. str
-            --   end,
-            node = "fuzzy"
-        },
-        jump = {
-            -- automatically jump when there is only one match
-            autojump = true,
-        },
-        label = {
-            uppercase = false,
-        },
-        modes = {
-            search = {
-                -- when `true`, flash will be activated during regular search by default.
-                -- You can always toggle when searching with `require("flash").toggle()`
-                enabled = false,
+    {
+        -- 支持中文查询
+		"rainzm/flash-zh.nvim",
+		event = "VeryLazy",
+		dependencies = "folke/flash.nvim",
+		keys = {
+			{ "s", mode = { "n", "x", "o" },
+				function()
+					require("flash-zh").jump({chines_only = false})
+				end,
+				desc = "Flash between Chinese",
+			},
+            { "o", mode = "o",
+                function() require("flash-zh").jump({
+                    -- 全屏搜索
+                    search = { wrap = true },
+                    remote_op = {
+                        restore = true,
+                        motion = true,
+                    },
+                }) end,
+                desc = "Remote Flash"
             },
-            char = {
-                enabled = false,
-                multi_line = false,
-                autohide = false
-            }
+		},
+	},
+    {
+        "folke/flash.nvim",
+        ---@type Flash.Config
+        opts = {
+            search = {
+                -- * exact: exact match
+                -- * search: regular search
+                -- * fuzzy: fuzzy search
+                -- * fun(str): custom function that returns a pattern
+                --   For example, to only match at the beginning of a word:
+                --   mode = function(str)
+                --     return "\\<" .. str
+                --   end,
+                node = "fuzzy"
+            },
+            jump = {
+                -- automatically jump when there is only one match
+                autojump = true,
+            },
+            label = {
+                uppercase = false,
+            },
+            modes = {
+                search = {
+                    -- when `true`, flash will be activated during regular search by default.
+                    -- You can always toggle when searching with `require("flash").toggle()`
+                    enabled = false,
+                },
+                char = {
+                    enabled = false,
+                    multi_line = false,
+                    autohide = false
+                }
+            },
         },
-    },
 
-    -- stylua: ignore
-    keys = {
-        -- { "<c-s>", mode = { "n", "o", "x" }, function() require("flash").jump() end,              desc = "Flash" },
-        { "s",     mode = { "n", "o", "x" }, function() require("flash").jump() end,              desc = "Flash" },
-        { "S",     mode = { "n", "o", "x" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-        { "o",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-        { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-        { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
-        {
-            "<leader>jl",
-            mode = { "n", "o", "x" },
-            function()
-                require("flash").jump({
-                    search = { mode = "search", max_length = 0 },
-                    label = { uppercase = true, after = { 0, 0 } },
-                    pattern = "^[ \t]*",
-                    jump = { pos = "end" },
-                })
-            end,
-            desc = "[J]ump [L]ine"
+        -- stylua: ignore
+        keys = {
+            -- { "<c-s>", mode = { "n", "o", "x" }, function() require("flash").jump() end,              desc = "Flash" },
+            -- { "s",     mode = { "n", "o", "x" }, function() require("flash").jump() end,              desc = "Flash" },
+            { "S",     mode = { "n", "o", "x" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+            -- { "o",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+            { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+            {
+                "<leader>jl",
+                mode = { "n", "o", "x" },
+                function()
+                    require("flash").jump({
+                        search = { mode = "search", max_length = 0 },
+                        label = { uppercase = true, after = { 0, 0 } },
+                        pattern = "^[ \t]*",
+                        jump = { pos = "end" },
+                    })
+                end,
+                desc = "[J]ump [L]ine"
+            },
+            {
+                "<leader>jw",
+                mode = { "n", "o", "x" },
+                function()
+                    require("flash").jump({
+                        pattern = ".", -- initialize pattern with any char
+                        search = {
+                            mode = function(pattern)
+                                -- remove leading dot
+                                if pattern:sub(1, 1) == "." then
+                                    pattern = pattern:sub(2)
+                                end
+                                -- return word pattern and proper skip pattern
+                                return ([[\<%s\w*\>]]):format(pattern), ([[\<%s]]):format(pattern)
+                            end,
+                        },
+                        jump = {
+                            ---@type "start" | "end" | "range"
+                            pos = "start"
+                        },
+                    })
+                end,
+                desc = "[J]ump [W]ord"
+            },
+            {
+                "<leader>jW",
+                mode = { "n", "o" },
+                function()
+                    require("flash").jump({
+                        pattern = ".", -- initialize pattern with any char
+                        search = {
+                            mode = function(pattern)
+                                -- remove leading dot
+                                if pattern:sub(1, 1) == "." then
+                                    pattern = pattern:sub(2)
+                                end
+                                -- return word pattern and proper skip pattern
+                                return ([[\<%s\w*\>]]):format(pattern), ([[\<%s]]):format(pattern)
+                            end,
+                        },
+                        jump = {
+                            ---@type "start" | "end" | "range"
+                            pos = "range"
+                        },
+                    })
+                end,
+                desc = "[J]ump [W]ord and select"
+            },
         },
-        {
-            "<leader>jw",
-            mode = { "n", "o", "x" },
-            function()
-                require("flash").jump({
-                    pattern = ".", -- initialize pattern with any char
-                    search = {
-                        mode = function(pattern)
-                            -- remove leading dot
-                            if pattern:sub(1, 1) == "." then
-                                pattern = pattern:sub(2)
-                            end
-                            -- return word pattern and proper skip pattern
-                            return ([[\<%s\w*\>]]):format(pattern), ([[\<%s]]):format(pattern)
-                        end,
-                    },
-                    jump = {
-                        ---@type "start" | "end" | "range"
-                        pos = "start"
-                    },
-                })
-            end,
-            desc = "[J]ump [W]ord"
-        },
-        {
-            "<leader>jW",
-            mode = { "n", "o" },
-            function()
-                require("flash").jump({
-                    pattern = ".", -- initialize pattern with any char
-                    search = {
-                        mode = function(pattern)
-                            -- remove leading dot
-                            if pattern:sub(1, 1) == "." then
-                                pattern = pattern:sub(2)
-                            end
-                            -- return word pattern and proper skip pattern
-                            return ([[\<%s\w*\>]]):format(pattern), ([[\<%s]]):format(pattern)
-                        end,
-                    },
-                    jump = {
-                        ---@type "start" | "end" | "range"
-                        pos = "range"
-                    },
-                })
-            end,
-            desc = "[J]ump [W]ord and select"
-        },
-    },
+    }
 }
